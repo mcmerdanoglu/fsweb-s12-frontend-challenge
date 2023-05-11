ab493cfa0e3f2ca12b854f95124bcf9a97054ff2
mcmerdanoglu <mcmerdanoglu@gmail.com>
2023-05-11 21:33:29 +0300

    Footera kadar gelindi

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,3 +1,48 @@
 export default function Profile() {
-  return <div>Profile</div>;
+  const basicInfo = {
+    Birth: "24.03.1996",
+    City: "Ankara",
+    Graduate: "Gazi Uni. Public Finanace, Bachelor, 2005",
+    Occupation: "Full stack dev",
+  };
+
+  return (
+    <div className="profile-mainbox">
+      <h2 className="profile-title">Profile</h2>
+      <div className="profile-elements">
+        <div className="basic-info">
+          <h4>Basic Information</h4>
+          <div className="info">
+            <div className="questions">
+              <p>Date of Birth</p>
+              <p>City of Residence</p>
+              <p>Graduate Level</p>
+              <p>Preffered Role</p>
+            </div>
+            <div className="answers">
+              <p>24.03.1996</p>
+              <p></p>
+              <p>Ankara</p>
+              <p>Hacettepe Ünv. Biyoloji Lisans, 2016</p>
+              <p>Frontend, UI</p>
+            </div>
+          </div>
+        </div>
+        <img src="https://media.istockphoto.com/id/1194430826/photo/software-developer-programming-finding-solutions-while-working-on-desktop-computers-in-data.jpg?s=170667a&w=0&k=20&c=qgUc8LvKMcAqcZVJ9YTdSorwC90xiEls_HigGULk1jE=" />
+        <div className="about">
+          <h4>About Me</h4>
+          <div className="lorem">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+              aut, odit laborum aliquam voluptatum nisi mollitia.{" "}
+            </p>
+            <p>
+              Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod
+              deserunt quam temporibus cumque magnam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,56 @@
+import Diziler from "../screenshots/diziler2.png";
+import Filmler from "../screenshots/filmler.png";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects-mainbox">
+      <h2>Projects</h2>
+      <div className="project-box up">
+        <img src={Diziler} alt="dizi-project" className="rectangle" />
+        <div className="project-explanation">
+          <h4>Ne izlesem?</h4>
+          <p>
+            A simple, customizable, minimal setup cookie plugin that allows your
+            users to select which cookies to accept or decline. This was created
+            with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
+            package and the git repository makes any type of customization to
+            code and themes possible.
+          </p>
+          <div className="button-box">
+            <button>react</button>
+            <button>redux</button>
+            <button>vercel</button>
+          </div>
+          <div className="project-links">
+            <a href="https://ne-izlesem.vercel.app">View Site</a>
+            <a href="https://github.com/mcmerdanoglu/ne-izlesem">Github</a>
+          </div>
+        </div>
+      </div>
+      <div className="project-box down">
+        <img src={Filmler} alt="film-project" className="rectangle" />
+        <div className="project-explanation">
+          <h4>Crud Filmler</h4>
+          <p>
+            A simple, customizable, minimal setup cookie plugin that allows your
+            users to select which cookies to accept or decline. This was created
+            with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
+            package and the git repository makes any type of customization to
+            code and themes possible.
+          </p>
+          <div className="button-box">
+            <button>react</button>
+            <button>redux</button>
+            <button>vercel</button>
+          </div>
+          <div className="project-links">
+            <a href="https://crud-filmler-g3.vercel.app">View Site</a>
+            <a href="https://github.com/mcmerdanoglu/fsweb-s11g3-crud-filmler">
+              Github
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
